cadfe3027f5b1adf22cf25b1affaf7afc4011b05
Thauan <83513806+Thauan-th@users.noreply.github.com>
2022-06-07 16:24:14 -0300

    using auth

--- App.js
+++ App.js
@@ -14,47 +14,31 @@ import firebase from './src/config/firebase'
 import { FlatList } from 'react-native';
 
 export default function App() {
-  const [nome,setNome] = React.useState('')
+  const [senha,setSenha] = React.useState('')
   const [email,setEmail] = React.useState('')
-  const [usuarios,setUsuarios]= React.useState([])
-  const [load,setLoad] = React.useState(true)
+  const [nome,setNome] = React.useState('')
 
   const fb =  firebase.database().ref('usuarios')
-  React.useEffect(()=>{
-    getUsers()
-  },[])
-  React.useEffect(()=>{
-    if(usuarios.length>0) setLoad(false)
-  },[usuarios])
-  
-  async function getUsers(){
-      let aux = []
-        await fb.once('value',snp => {
-          snp.forEach(item=>{
-            let data = {
-              key : item.key,
-              nome: item.val().nome,
-              email: item.val().email
-            }
-            aux.push(data)
-          })
-          setUsuarios(aux)
-        })
-    }
-    
-    async function cadastrar(){
-      if(nome.length ===0 &&  email.length ===0 ){
+
+  async function Logar(){
+      if(senha.length ===0 &&  email.length ===0 ){
         return alert('Preencha corretamente os campos')
       }
-
-    const key = fb.push().key
-    fb.child(key).set({
-      nome,
-      email
-    })
-    alert('Cadastrado')
+      await firebase.auth().createUserWithEmailAndPassword(email,senha).then(value=>{
+        fb.child(value.user.uid).set({
+          nome,
+          email,
+          senha
+        })
+      }).catch(err=>{
+        if(err)console.log(err) 
+      })
+  }
+  async function logOut(){
+    alert('deslocado')
+    setUser('')
+    setSenha('')
     setEmail('')
-    setNome('')
   }
 
 
@@ -64,22 +48,9 @@ export default function App() {
       <TextInput value={nome} onChangeText={e=>setNome(e)} style={styles.input} />
       <Text>Email</Text>
       <TextInput value={email} onChangeText={e=>setEmail(e)} style={styles.input} />
-      <Button title='Cadastrar' onPress={cadastrar}/>
-      {load ? <ActivityIndicator/> :
-      <FlatList
-      keyExtractor={item=>item.key}
-      data={usuarios}
-      renderItem={({item})=>{
-        return (
-              <>
-          -
-          <Text>{item.nome}</Text>
-          <Text>{item.email}</Text>
-          </> 
-         )
-        }}
-        />
-      }
+      <Text>Senha</Text>
+      <TextInput value={senha} onChangeText={e=>setSenha(e)} style={styles.input} />
+      <Button title='Logar' onPress={Logar}/>
       <StatusBar style="auto" hidden={true} />
     </SafeAreaView>
   );
